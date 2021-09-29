135261dc40febd8170ad72353c6d43c038182e1a
Zach Ghera <zpghera00@gmail.com>
2021-09-28 21:23:56 -0400

    Create toy scene for visualizing raw imu data.

---
+++ Communication-and-Sensor-Fusion-Prototyping/Assets/Scripts/ValueDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ValueDisplay : MonoBehaviour {
+  public string zachIsPoop;
+  public GameObject g;
+
+  // Start is called before the first frame update
+  void Start() {
+    //g = transform.
+    //  ValueDisplay vd = g.GetComponent<ValueDisplay>();
+
+    //if (vd == null) {
+    //  Debug.LogError("IT BROKE");
+    //  this.enabled = false;
+    //}
+  }
+
+  // Update is called once per frame
+  void Update() {
+
+  }
+}
