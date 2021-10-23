3b4ec94250d3501ef4a2a316127638266281c3b8
Zach Ghera <zpghera00@gmail.com>
2021-10-23 19:36:41 -0400

    Created compass demo.
    Attempt to determine if the yaw issue lied in the ahrs update function. It did not as this scene displayed the same behavior as the madgwick cube demo.

--- Communication-and-Sensor-Fusion-Prototyping/Assets/Scripts/Controller.cs
+++ Communication-and-Sensor-Fusion-Prototyping/Assets/Scripts/Controller.cs
@@ -2,14 +2,18 @@
 using UnityEngine.Assertions;
 using System.Threading;
 
+public enum Scenes {
+  VALDISPLAY, CUBE, COMPASS
+}
+
 public class Controller : MonoBehaviour {
   public SerialReader reader;
   public Madgwick fusion;
   public ValueDisplay accelDisplay;
   public ValueDisplay gyroDisplay;
   public ValueDisplay magDisplay;
-  public Transform cubeTf;
-  public bool isMadgwickDemo = false;
+  public Transform tf;
+  public Scenes scene;
   private bool _firstUpdate = true;
   private float _timeSinceLastPacketS = 0; // sec
 
@@ -19,7 +23,7 @@ public class Controller : MonoBehaviour {
     } catch (HardwareConfigurationException) {
       throw;
     }
-    if (isMadgwickDemo) {
+    if (scene != Scenes.VALDISPLAY) {
       fusion = new Madgwick();
     }
     reader.WaitUntilReady();
@@ -35,18 +39,29 @@ public class Controller : MonoBehaviour {
     _timeSinceLastPacketS += Time.deltaTime;
     try {
       ImuSample sample = reader.GetImuSamples();
-      if (isMadgwickDemo) {
-        fusion.Update(sample.AngVel, sample.LinAccel, 
-                      sample.MagField, _timeSinceLastPacketS);
-        _timeSinceLastPacketS = 0;
+      switch (scene) {
+        case Scenes.VALDISPLAY: {
+          accelDisplay.UpdateValue(sample.LinAccel);
+          gyroDisplay.UpdateValue(sample.AngVel);
+          magDisplay.UpdateValue(sample.MagField);
+          break;
+        }
+        case Scenes.CUBE: {
+          fusion.AhrsUpdate(sample.AngVel, sample.LinAccel, 
+                        sample.MagField, _timeSinceLastPacketS);
+          _timeSinceLastPacketS = 0;
 
-        var q = fusion.GetQuaternion();
-        cubeTf.rotation = new Quaternion(q.x, q.z, q.y, q.w);
-        Debug.LogWarning($"x={q.x}, y={q.z}, z={q.y}, w={q.w}");
-      } else {
-        accelDisplay.UpdateValue(sample.LinAccel);
-        gyroDisplay.UpdateValue(sample.AngVel);
-        magDisplay.UpdateValue(sample.MagField);
+          var q = fusion.GetQuaternion();
+          tf.rotation = new Quaternion(q.x, q.z, q.y, q.w);
+          Debug.LogWarning($"x={q.x}, y={q.z}, z={q.y}, w={q.w}");
+          break;
+        }
+        case Scenes.COMPASS: {
+          tf.rotation = Quaternion.Euler(0, 0, fusion.CompassUpdate(
+              sample.LinAccel, sample.MagField));
+          break;
+        }
+        default: throw new System.Exception();
       }
     } catch (PacketQueueEmptyException e) {
       Debug.Log(e);
